9f9b983b20ac330dff32cbb6f0a94f71b147b1ff
shrikantdeshmukh9766-ux <shrikantdeshmukh9766@gmail.com>
2026-03-10 21:44:58 +0530

    app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from koboextractor import KoboExtractor
 st.title("ASHA Form Submission Dashboard")
 
 # Kobo credentials
-my_token = "from koboextractor import KoboExtractor
-import pandas as pd
 
 my_token = "23801d339dd6d16509a79250731f126401d5f7a3"
 form_id = "afWux6DQFqmZrEpK54BobD"
@@ -92,3 +90,4 @@ st.dataframe(asha_summary, use_container_width=True)
 st.subheader("Forms Filled by ASHA")
 
 st.bar_chart(asha_summary.set_index("asha"))
+
